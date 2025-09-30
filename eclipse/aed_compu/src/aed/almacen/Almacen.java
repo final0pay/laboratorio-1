7fa46e39fd38d84b653f6f8e58ec41f467b2c872
final0pay <dayronaguilarmeneces1@gmail.com>
2025-09-30 13:44:20 +0200

    primer commit

---
+++ eclipse/aed_compu/src/aed/almacen/Almacen.java
@@ -0,0 +1,28 @@
+package aed.almacen;
+
+import es.upm.aedlib.indexedlist.IndexedList;
+import es.upm.aedlib.indexedlist.ArrayIndexedList;
+
+
+/**
+ * Implementa la logica del almacen.
+ */
+public class Almacen implements ClienteAPI, AlmacenAPI, ProductorAPI {
+
+  // Compras (sin ningun orden especial)
+  private ArrayIndexedList<Compra> compras;
+  // Productos ordenados ascendamente usando el productoId de un Product.
+  private ArrayIndexedList<Producto> productos;
+
+  // No es necesario cambiar el constructor
+  /**
+   * Crea un almacen.
+   */
+  public Almacen() {
+    this.compras = new ArrayIndexedList<>();
+    this.productos = new ArrayIndexedList<>();
+  }
+
+
+  // Implementa los métodos necesarios aqui ...
+}
